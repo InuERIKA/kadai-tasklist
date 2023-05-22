2c01153789a29661ebbbe1715e3c2572ec8ba6e6
8Y47QQ2 <8Y47QQ2@192.168.103.243>
2023-05-22 10:59:14 +0900

    データベース関連のファイルを追加

---
+++ src/models/Task.java
@@ -0,0 +1,64 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+	//ID
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	//作成日時
+	@Column(name = "created_date", nullable = false)
+	private Timestamp created_date;
+
+	//更新日時
+	@Column(name = "updated_date", nullable = false)
+	private Timestamp updated_date;
+
+	//content
+	@Column(name = "content", length = 255, nullable = false)
+	private String content;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Timestamp getCreated_date() {
+		return created_date;
+	}
+
+	public void setCreated_date(Timestamp created_date) {
+		this.created_date = created_date;
+	}
+
+	public Timestamp getUpdated_date() {
+		return updated_date;
+	}
+
+	public void setUpdated_date(Timestamp updated_date) {
+		this.updated_date = updated_date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
