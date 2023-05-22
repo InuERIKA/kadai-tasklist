cd2c7ebb129b3e087186d91bbf2b8892eb0c5fc4
8Y47QQ2 <8Y47QQ2@192.168.103.243>
2023-05-22 15:59:55 +0900

    存在しないidに対する処理を追加

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -1,11 +1,16 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class DestroyServlet
@@ -13,22 +18,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/destroy")
 public class DestroyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public DestroyServlet() {
         super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -54,6 +51,5 @@ public class DestroyServlet extends HttpServlet {
             response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-	}
 
 }
